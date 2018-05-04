81d5abe3ffc88588ac19a889dc66a6b70b16f851
Bruno Pimentel <bpimentel@mastertech.tech>
2018-05-04 18:38:02 -0300

    Adiciona browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,12 @@ let gulp = require('gulp');
 let sass = require('gulp-sass');
 let concat = require('gulp-concat');
 let cleanCSS = require('gulp-clean-css');
+let browserSync = require('browser-sync').create();
 
 gulp.task('sass', function () {
     return gulp.src('./src/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./dist'));
-});
-
-gulp.task('sass:watch', function () {
-    gulp.watch('./src/scss/**/*.scss', ['sass']);
 });
 
 gulp.task('scripts', function() {
@@ -19,8 +16,12 @@ gulp.task('scripts', function() {
     .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('scripts:watch', function () {
-    return gulp.watch('./src/js/**/*.js', ['scripts']);
-});
+gulp.task('default', ['sass', 'scripts'], function() {
+    browserSync.init({
+        server: "./dist"
+    });
 
-gulp.task('default', ['sass', 'scripts', 'sass:watch', 'scripts:watch']);
+    gulp.watch('./src/scss/**/*.scss', ['sass']);
+    gulp.watch('./src/js/**/*.js', ['scripts']);
+    gulp.watch(['./dist/*']).on('change', browserSync.reload);
+});
