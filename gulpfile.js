a5d31dd6ab1eb798ec921427482a575c527753b7
Renan Rodrigo <renanrb@ime.usp.br>
2018-05-07 20:28:47 -0300

    Adicionar index.html no gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,11 @@ let concat = require('gulp-concat');
 let cleanCSS = require('gulp-clean-css');
 let browserSync = require('browser-sync').create();
 
+gulp.task('html', function() {
+    return gulp.src('./src/index.html')
+    .pipe(gulp.dest('./dist'));
+});
+
 gulp.task('sass', function () {
     return gulp.src('./src/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
@@ -16,12 +21,13 @@ gulp.task('scripts', function() {
     .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('default', ['sass', 'scripts'], function() {
+gulp.task('default', ['html', 'sass', 'scripts'], function() {
     browserSync.init({
         server: "./dist"
     });
 
     gulp.watch('./src/scss/**/*.scss', ['sass']);
     gulp.watch('./src/js/**/*.js', ['scripts']);
+    gulp.watch('./src/index.html', ['html']);
     gulp.watch(['./dist/*']).on('change', browserSync.reload);
 });
