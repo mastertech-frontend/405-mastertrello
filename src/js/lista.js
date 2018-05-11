7f995624137763f68a009fccc3cd8e4e995853d2
Bruno Pimentel <bpimentel@mastertech.tech>
2018-05-11 20:29:56 -0300

    Adiciona evento de keydown no input da lista

--- src/js/lista.js
+++ src/js/lista.js
@@ -7,7 +7,14 @@ class Lista{
         this.elementoHTML.appendChild(input);
 
         // outra forma de adicionar um elemento
-        // this.elementoHTML.innerHTML = '<input type="text">'; 
+        // this.elementoHTML.innerHTML = '<input type="text">';
+
+        input.onkeydown = (event) => {
+            if(event.key == 'Enter'){
+                this.adicionarCard(input.value);
+                input.value = '';
+            }
+        }
         
         document.body.appendChild(this.elementoHTML);
     }
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,1 @@
 let lista = new Lista();
-lista.adicionarCard('Renan tá de férias');
-lista.adicionarCard('Eu to com fome');
